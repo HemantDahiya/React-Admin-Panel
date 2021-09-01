9005d074605ab790a965a828b29f8bf2450cf7ef
HemantDahiya <hmntkmr46@gmail.com>
2021-09-01 23:15:08 +0530

    Update index.js
    Changes https for API call

--- src/Component/Data list/index.js
+++ src/Component/Data list/index.js
@@ -17,7 +17,7 @@ const AdminTable = ({ handleClick }) => {
     }
 
     useEffect(() => {
-        fetch("http://www.filltext.com/" + 
+        fetch("https://www.filltext.com/" + 
         "?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D" + 
         "&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D")
             .then(res => res.json())
